21845919182dda556f3246ae789de58a5a987ad2
Sylvia Chen <sylvialchen33@gmail.com>
2022-10-24 21:18:45 -0400

    update deployment

--- kpl_sister_nicknames/settings.py
+++ kpl_sister_nicknames/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-# import django_heroku
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,4 +128,4 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
