6dd7d7091ea9391b8d2540e9aeb94947e19dd819
Sylvia Chen <sylvialchen33@gmail.com>
2022-10-26 11:26:19 -0400

    deploy

--- kpl_sister_nicknames/settings.py
+++ kpl_sister_nicknames/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-# import django_heroku
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-SECRET_KEY = "django-insecure-k=zyzs&(kv#qd0p*p!dfr$6je8lvwmyjty9c$$zd2+=q-mtf67^-u*juj"
+SECRET_KEY = "django-insecure-k=zyzs&(kv#qd0p*p!dfrgit $6je8lvwmyjty9c$$zd2+=q-mtf67^-u*juj"
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
@@ -128,4 +128,4 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
